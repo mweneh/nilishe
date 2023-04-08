20c4751bf940803f80697ece3d3b99effa2e1c1b
David <davidongaroh@gmail.com>
2023-04-08 21:28:22 +0300

    refactor use of .first on the ActiveRecord

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,10 +2,14 @@ class AuthenticationController < ApplicationController
 
   def login_user
     user = User.where(["username = :username OR email = :email",
-                       {username: auth_params[:username], email: auth_params[:email]}] )
-    user.first&.authenticate(auth_params[:pass])? account_login(token: create_token(user)) : account_login(failed: true)
+                       {username: auth_params[:username], email: auth_params[:email]}] ).first
+    if user && user.authenticate(auth_params[:pass])
+      account_login(token: create_token(user))
+    else
+      account_login(failed: true)
+    end
   end
-
+  
   def register_user
     user = User.create(username: auth_params[:username], email: auth_params[:email], password: auth_params[:pass])
     user.valid? ? account_created(token: create_token(user)) : account_created(failed: true, errors: user.errors)
