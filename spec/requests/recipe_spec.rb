a2b54eae291d6f532254c8b78d55a5e85ce7411a
David <davidongaroh@gmail.com>
2023-04-08 21:42:06 +0300

    test for valid methods

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # check for valid methods in the controller
+  describe RecipeController, type: :controller do
+
+    # check for existence of methods in the controller
+    it { is_expected.to respond_to(:create_recipe) }
+    it { is_expected.to respond_to(:update_recipe) }
+    it { is_expected.to respond_to(:delete_recipe) }
+
   end
 end
