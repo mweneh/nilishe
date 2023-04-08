be3a0551b15f52daf7aa07c4717b94d5493e1e32
David <davidongaroh@gmail.com>
2023-04-08 13:12:46 +0300

    + email validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,26 @@ RSpec.describe User, type: :model do
   describe "model should have secure password" do
     it { should have_secure_password }
   end
+  describe "email data validations" do
+
+    describe "email in-built validators" do
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email) }
+    end
+  
+    describe "email format custom validator" do
+      it "should contain a valid email address" do
+        email_error = "That email is not valid"
+  
+        invalid_mail = User.create(email: "a.com")
+        valid_mail = User.create(email: "home@mail.com")
+  
+        expect(invalid_mail.errors[:base]).to include(email_error)
+        expect(valid_mail.errors[:base]).to_not include(email_error)
+      end
+    end
+  
+  end
 
 end
+
