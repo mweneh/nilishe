40f473658b24a347e0eac041e187d3f9e1d726e4
David <davidongaroh@gmail.com>
2023-04-08 22:29:05 +0300

    adjust routes_data for more info

--- spec/common/routes_data.rb
+++ spec/common/routes_data.rb
@@ -1,8 +1,28 @@
 module RoutesTestData
 
+    ## ROUTES
     # authentication routes
-    def auth
+    def r_auth
       %w[/auth/create_account /auth/login]
     end
+  
+    # recipe routes
+    def r_recipe(rid: 0)
+      %W[/recipe/create /recipe/update/#{rid} /recipe/delete/#{rid}]
+    end
+  
+    ## OTHER DATA
+    # define headers required when making requests
+    def headers
+     { 'ACCEPT' => "application/json" }
+    end
+  
+    # add authentication token to headers
+    def headers_with_token(token)
+      tmp_headers = headers
+      tmp_headers['Authorization'] = "Bearer #{token}"
+      tmp_headers
+    end
+  
   
   end
