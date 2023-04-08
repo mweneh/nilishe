5497a263b63be743012ff9c2f441f919246b586b
David <davidongaroh@gmail.com>
2023-04-08 12:56:49 +0300

    add foreign keys

--- db/migrate/20230408095338_create_recipes.rb
+++ db/migrate/20230408095338_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :title
-      t.integer :user
+      t.belongs_to :user, foreign_key: true
       t.string :description
 
       t.timestamps
--- db/migrate/20230408095407_create_meals.rb
+++ db/migrate/20230408095407_create_meals.rb
@@ -1,13 +1,13 @@
 class CreateMeals < ActiveRecord::Migration[7.0]
   def change
     create_table :meals do |t|
-      t.integer :country
+      t.belongs_to :country, foreign_key: true
       t.string :description
-      t.integer :recipe
+      t.belongs_to :recipe, foreign_key: true
       t.string :tags
       t.string :ingredients
-      t.integer :category
-      t.integer :status
+      t.belongs_to :category, foreign_key: true
+      t.integer :status, default: 0
 
       t.timestamps
     end
