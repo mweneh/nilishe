c6bcd5d18e680207f9f714b08ca42e9e74a110d0
David <davidongaroh@gmail.com>
2023-04-08 13:14:55 +0300

    refactor code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,40 +12,42 @@ RSpec.describe User, type: :model do
       it { is_expected.to validate_presence_of(:email) }
       it { is_expected.to validate_uniqueness_of(:email) }
     end
-  
+
     describe "email format custom validator" do
       it "should contain a valid email address" do
         email_error = "That email is not valid"
-  
+
         invalid_mail = User.create(email: "a.com")
         valid_mail = User.create(email: "home@mail.com")
-  
+
         expect(invalid_mail.errors[:base]).to include(email_error)
         expect(valid_mail.errors[:base]).to_not include(email_error)
       end
     end
-  
+
   end
+
 
   describe "username data validations" do
 
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_uniqueness_of(:username) }
     it { is_expected.to validate_length_of(:username).is_at_least(6) }
-  
+
   end
+
 
   describe "password data validation" do
     it "should contain a valid email address" do
       password_error = "That password is not strong enough"
-  
+
       invalid_password = User.create(password: "pas12")
       valid_password = User.create(password: "S@mail2")
-  
+
       expect(invalid_password.errors[:base]).to include(password_error)
       expect(valid_password.errors[:base]).to_not include(password_error)
     end
   end
 
-end
 
+end
