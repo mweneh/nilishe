8acf7e0c6a7da91c847f96aae664ff007177eed6
David <davidongaroh@gmail.com>
2023-04-08 13:27:45 +0300

    add tests for auth

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,7 +1,65 @@
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe AuthenticationController,  type: :controller do
+
+  describe "Controller has valid methods" do
+    it { is_expected.to respond_to(:login_user) }
+    it { is_expected.to respond_to(:register_user) }
   end
+
+  # Specs to check whether login and create account operations
+  describe "Authentication", type: :request do
+
+    # define routes for authentication
+    create_account = "/auth/create_account"
+    login_account = "/auth/login"
+
+    # valid random user properties
+    username = "mamanilishe"
+    email = "app@nilishe.com"
+    password = "Complexity.!123"
+
+    # define headers required when making requests
+    headers = { 'ACCEPT' => "application/json" }
+
+    it "handles account creation properly" do
+      # no params
+      post create_account, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      # provide username, email and password that are valid
+      post create_account, params: { username: username, email: email, pass: password }, headers: headers
+      expect(response).to have_http_status(:created)
+
+      # provide invalid data
+      post create_account, params: { username: "hi", email: "app", pass: "123456" }, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      # provide duplicate data
+      post create_account, params: { username: username, email: email, pass: password }, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+
+    end
+
+    it "handles account sign in properly" do
+      # create a user account
+      User.create(username: username, email: email, password: password)
+
+      # no params
+      post login_account, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      # correct account details (username/password)
+      post login_account, params: { username: username, pass: password }, headers: headers
+      expect(response).to have_http_status(:ok)
+
+      # correct details (email/password)
+      post login_account, params: { email: email, pass: password }, headers: headers
+      expect(response).to have_http_status(:ok)
+
+
+    end
+
+  end
+
 end
