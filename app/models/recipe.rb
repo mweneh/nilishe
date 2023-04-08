596aa4515b1fb1a2ee02a9b9bc82e1fbfcb4b8d5
David <davidongaroh@gmail.com>
2023-04-08 22:18:06 +0300

    validate title & description

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
     belongs_to :user
     has_one :meal
+    validates :title, { length: { minimum: 3 }, presence: true }
+    validates :description, { length: { minimum: 15 }, presence: true }
   end
