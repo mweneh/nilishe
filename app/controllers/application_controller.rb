3b8a608f6f039c3fbd7d6f66291bc343d6dcbd32
David <davidongaroh@gmail.com>
2023-04-08 15:27:07 +0300

    + encode & decode methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::API
     # jwt secret key
 SECRET_KEY = ENV['JWT_SECRET']
+
+  # JWT Encoding
+  def encode_data(payload)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
+  end
+
+  # JWT Decoding
+  def decode_data(token)
+    begin
+      JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })
+    rescue JWT::DecodeError => e
+      nil
+    end
+  end
 end
