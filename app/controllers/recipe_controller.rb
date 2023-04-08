e2e5a5072585416ad3006e8f588ac95643a7e146
David <davidongaroh@gmail.com>
2023-04-08 22:37:05 +0300

    + methods for req params & action responses

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,3 +1,32 @@
 class RecipeController < ApplicationController
     before_action :is_authorized?
+    
+    private
+
+def recipe_params
+    params.permit(:title, :description)
+end
+
+def recipe_created(success: true, recipe: nil)
+    app_response(
+      status: success ? :created : :unprocessable_entity,
+      message: success ? "Recipe has been created!" : "Could not create recipe",
+      body: { :recipe => recipe}
+    )
+  end
+
+  def recipe_updated(success: true, recipe: nil)
+    app_response(
+      status: success ? :ok : :unprocessable_entity,
+      message: success ? "Recipe has been updated!" : "Could not update recipe",
+      body: { :recipe => recipe}
+    )
+  end
+
+  def recipe_deleted(success: true)
+    app_response(
+      status: success ? :ok : :unprocessable_entity,
+      message: success ? "Recipe has been deleted!" : "Could not delete recipe"
+    )
+  end
 end
