64b9bf0acd0b235e6306e989e27a4643b59017ab
David <davidongaroh@gmail.com>
2023-04-17 19:38:01 +0300

    add recipe/all routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
    post "/recipe/create", to: "recipe#create_recipe"
    put "/recipe/update/:id", to: "recipe#update_recipe"
    delete "/recipe/delete/:id", to: "recipe#delete_recipe"
+
+   get "/recipe/all", to: "recipe#fetch_recipes"
+   get "/recipe/all/:q", to: "recipe#search_recipes"
 end
